90bac7218b5d9b0f696123b0888a57bf1ab996be
kperkins97 <perkins.kelsey@yahoo.com>
2021-09-26 14:45:57 -0500

    trying create save button function so user can save info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
-var today = moment().format("MMMM Do, YYYY, HH:mm"); 
+// displays current date and time
+var today = moment().format("MMMM Do, YYYY, HH:mm A"); 
 $("#currentDay").text(today);
 
-// var dateTime = moment.format("MMMM Do, YYYY, hh:mm");
-// $("currentDay").text(today);
+var saveBtn = document.querySelectorAll(".saveBtn")
+//var storedItem = localStorage.getItem("storedItem");
+
+//where user will type schedule stuff
+var textArea = document.querySelectorAll(".flex-grow-1")
+
+function savedUserInfo() {
+    saveBtn.values;
+    var storedItem = localStorage.getItem('storedItem');
+
+    textArea.textContent = storedItem;
+
+}
+
+
+
+//might need to take out the "s" on .value on line 25
+//took out "event" in function ()
+saveBtn.addEventListener("click", function() {
+    // event.preventDefault();
+     saveBtn.values;
+     console.log("saving user info")
+
+    if (textArea == "") {
+        alert("Oops, You forgot to type something!");
+     } else {
+        alert("Your schedule is now saved!");
+
+         localStorage.setItem("saveBtn", saveBtn);
+         savedUserInfo();
+     }
+});
