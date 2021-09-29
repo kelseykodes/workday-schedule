40cd626021e01d6da5bd68190a29b2441bcd2800
kperkins97 <perkins.kelsey@yahoo.com>
2021-09-28 21:04:32 -0500

    able to render user input/text content

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,43 +4,37 @@ $("#currentDay").text(today);
 
 //grabing all 9 'save' buttons 
 var saveBtn = document.querySelectorAll(".saveBtn")
-console.log(saveBtn)
+
+
 //where user will type schedule stuff
 var textArea = document.querySelectorAll(".textarea")
 console.log(textArea);
-//textArea.textContent = storedItem;
 
 
 
 function renderSchedule() {
-    //variable declared to get user events in local storage
-    var storedItem = JSON.parse(localStorage.getItem('storedItem')) || {
-        event:[],
-        hour:[],
-    };
-
-    //textArea.textContent = storedItem;
-    //do the same like the buttons and loop. use values that i store in object 
-
-
+    for (var i = 9; i <=17; i++){
+        var savedData = localStorage.getItem(i);
+        document.getElementById(i + "hour").value = savedData
+    }
     //grabs each saveBtn on HTML and should store schedule stuff in each time-block
     for (var i = 0; i<saveBtn.length; i++) {
         //console.log(saveBtn[i])
+
         saveBtn[i].addEventListener("click", function(event) {
             // var dataHour determines what time block todoText is inside of
             var dataHour = event.target.dataset.hour;
             console.log(dataHour);
+
             // var todoText grabs text of user schedule input
             var todoText = document.getElementById(dataHour + "hour").value.trim()
             console.log(todoText);
-    
-                //displaying storedItem on web page
-                storedItem.event.push(textArea);
-                storedItem.hour.push(dataHour);
-                localStorage.setItem("storedItem",JSON.stringify(storedItem));
-        
-            if (textArea === " ") {
-               alert("Oops, You forgot to type something!"); //alert not working
+
+            //gives local storage key and value
+             localStorage.setItem(dataHour, todoText);
+
+            if (textArea == "") {
+               alert("Oops! You forgot to type something!"); //alert not working
              } else {
                alert("Your schedule is now saved!");
             };
@@ -51,70 +45,26 @@ function renderSchedule() {
 
 renderSchedule();
 
-
-
-
-
 //----crappy code below-------
+     
+
+//changing timeblock colors when dataHour === HH
+
+var hour = moment().format("HH"); 
+//console.log(hour);
 
 
+var timeBlocks = $('[data-hour]')
+console.log(timeBlocks);
 
-
-
-
-
-// saveBtn[i].addEventListener("click", function() {
-//     console.log(textArea[i].value);
-//     // event.preventDefault();
-//      saveBtn.values;
-//      console.log("saving user info")
-
-//     if (textArea == "") {
-//         alert("Oops, You forgot to type something!");
-//      } else {
-//         alert("Your schedule is now saved!");
-//         console.log(saveBtn);
-//          localStorage.setItem("saveBtn", saveBtn); //also tried saveBtn still nothing
-        
-//      };
-
-// });       
-
-//might need to take out the "s" on .value on line 25
-//took out "event" in function ()
-// // saveBtn[0].addEventListener("click", function() {
-//     //event.preventDefault();
-//      saveBtn.values;
-//      console.log("saving user info")
-
-//     if (textArea == "") {
-//         alert("Oops, You forgot to type something!");
-//      } else {
-//         alert("Your schedule is now saved!");
-
-//          localStorage.setItem("saveBtn", saveBtn);
-//          savedUserInfo();
-//      };
-// });
-
-
-
-//changing timeblock colors when specific times hit ,  THIS IS DRIVING ME INSANE
-//var hour = today.format("HH:mm A")
-
-
-// var timeBlocks = $('[data-time]')
-// console.log(timeBlocks);
-
-// for (var i =0; i<timeBlocks.length; i++) {
+for (var i = 9; i< 17; i++) {
     
 //     //grabs specific block
-//     var oneBlock = timeBlocks[i];
-//     //data-time is only 09 & 10
-//     var dataTime = oneBlock.getAttribute('data-time')
-    
+    var dataTime = singleBlock.getAttribute('data-hour')
+    var singleBlock = 
 //     //checks if that time block is in the past
-// if (dataTime < hour) {
+     if (dataTime < hour) {
+        document.querySelectorAll("textarea")[i].style.backgroundColor = "yellow";
    
 //     //changes background color
 //     oneBlock.style.backgroundColor='#350002'
@@ -123,4 +73,5 @@ renderSchedule();
 //     //document.querySelectorAll("textarea")[i].style.color = "black";
 //     //document.querySelectorAll("button")[i].style.color = "black";
 //     //document.querySelectorAll("button")[i].style.background= '#350002';
-// };}
+     }
+};
